d2ef3dae1a81ccbb43cd9f1eb1ffe595cb324220
Tim Lestander <timlestander@gmail.com>
2021-09-13 21:33:21 +0200

    fix: use heroku env port for server

--- server/index.js
+++ server/index.js
@@ -11,16 +11,11 @@ const leagueRouter = require('./routes/league.routes');
 const typeRouter = require('./routes/type.routes');
 
 const app = express()
-const apiPort = 8000
+const apiPort = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
-
-app.use(express.static(path.join(__dirname, '../client/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build'))
-})
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
@@ -29,5 +24,10 @@ app.use('/api', betRouter);
 app.use('/api', betterRouter);
 app.use('/api', leagueRouter);
 app.use('/api', typeRouter);
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build'))
+})
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
